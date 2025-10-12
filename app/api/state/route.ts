00ece2a86234769d2a492d41d46c16a93c308589
Shahm Najeeb <Nirt_12023@outlook.com>
2025-10-12 18:46:51 +0400

    Refactor database connection to use dbPool data const rather than reinit it

--- app/api/state/route.ts
+++ app/api/state/route.ts
@@ -1,21 +1,11 @@
-import {Pool} from 'pg';
-import fs from 'fs';
-import path from 'path';
 import {createSecureResponse} from '@/lib/utils';
 import {state} from "@/lib/data/api";
+import {dbPool} from "@/lib/db";
 
-
-const pool = new Pool({
-    connectionString: process.env.POSTGRES_URL,
-    ssl: {
-        rejectUnauthorized: true, // Enforce cert validation
-        ca: fs.readFileSync(path.join(process.cwd(), 'certs', 'ca.pem')).toString(),
-    },
-});
 
 export async function GET() {
     try {
-        const client = await pool.connect();
+        const client = await dbPool.connect();
         const res = await client.query('SELECT browser, clicked FROM button_states;');
         client.release();
         return createSecureResponse(res.rows);
