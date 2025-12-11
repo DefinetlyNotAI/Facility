8e969f33432095612995d0f32e3f3e3d65822d8f
Shahm Najeeb <Shahm_Najeeb@outlook.com>
2025-12-11 20:05:18 +0400

    Clarify instructions for Chapter IV numpad puzzle to enhance user understanding
    Signed-off-by: Shahm Najeeb <Shahm_Najeeb@outlook.com>

--- lib/data/chapters.public.ts
+++ lib/data/chapters.public.ts
@@ -119,7 +119,7 @@ export const chapterIVPublic = {
                 {
                     stage: 4,
                     title: 'Decaying Keypad',
-                    instruction: '5 number password. The sequence is based of word of the day: length, vowels, unique-letters, first-letter position in the alphabet, syllables.',
+                    instruction: '5 number password. The sequence is based of word of the day: length of this password, vowels in the word, unique-letters in the word, first-letter position in the alphabet, syllables count.',
                     payload: '52633',
                     type: 'numpad',
                 },
