0d84808b65974a50260e4428803af7e8e4b27127
Shahm Najeeb <Shahm_Najeeb@outlook.com>
2025-12-23 18:48:00 +0400

    Reformat code
    Signed-off-by: Shahm Najeeb <Shahm_Najeeb@outlook.com>

--- lib/client/data/TAScript.ts
+++ lib/client/data/TAScript.ts
@@ -3,62 +3,62 @@ import {TASType} from "@/types";
 
 export const TAScript: TASType = {
     pageHints: {
-    '/': [
-        "Hey there! Welcome to our little facility. Fair warning - once you click accept, we're both in this together.",
-        "Those content warnings? Yeah, they're not kidding around. But hey, I'll be here to help you through it!",
-        "Audio permissions are pretty important here. Trust me, you'll want to hear what's coming."
-    ],
-    '/home': [
-        "Welcome to your new home away from home! Don't mind the ominous research logs. They're just... atmospheric.",
-        "If you ever stumble here from another room, know that you have to do something here to continue, for there are many doors here!",
-        "Those research logs aren't just decoration - they're breadcrumbs. But who can promise they are not fabricated... Maybe wait to get actual archived ones?",
-        "See that countdown timer? It's not just for show. Time works differently here, friend.",
-        "The facility's keeping track of your visits. Don't worry, I think it likes you!"
-    ],
-    '/wifi-panel': [
-        "Two buttons... huh",
-        "When you get that transmission error, don't panic - it's asking for a specific type of encoding.",
-        "The numbers are important here. Think about what they might add up to.",
-    ],
-    '/wifi-login': [
-        "Username and password time! The credentials might be hiding in plain sight around the facility.",
-        "Maybe the characters limit for the password is a clue... think about it.",
-        "The username and password are not just random - they relate to my creator.",
-    ],
-    '/media': [
-        "Three items to interact with - an audio file, and two downloadable files. And a secret door unlocks.",
-        "That morse code audio? Listen carefully, or find a way to decode it. It's a key.",
-        "The ZIP files are password protected with the keywords you've been collecting. Use them wisely!",
-        "One ZIP leads to the other - it's a chain of chIV. Follow the breadcrumbs.",
-        "Each interaction unlocks the next step. It's all connected, trust me."
-    ],
-    '/file-console': [
-        "Welcome to the terminal! Try 'help' to explore available commands.",
-        "Some files can be downloaded with 'wget' - you might need them!",
-        "That robots.txt file is particularly interesting. Web crawlers aren't the only ones reading it.",
-        "The riddle PDF and hint file might be special. Download them both!",
-        "Invoke the shell, find two truths, one of identity and one of power."
-    ],
-    '/buttons': [
-        "Five browsers, but you can only press the one matching yours. It's a global system!",
-        "Once all browsers are pressed by different people, something special unlocks and it becomes a easy trail.",
-        "This is a collaborative puzzle. A one of a kind, The hub that connects all... wait, I shouldn't say that yet.",
-    ],
-    '/black-and-white': [
-        "Two QR codes... one's telling the truth, one's lying. A classic puzzle!",
-        "Want to find the moon? Type the hidden number and be lucky.",
-        "Listen well, use your ears.",
-        "The screen size matters for the final unlock.",],
-    '/h0m3': [
-        "...",
-    ],
-    '/scroll': [
-        "...",
-    ],
-    '/choices': [
-        "...",
-        "I have a bad feeling here.",
-    ]
+        '/': [
+            "Hey there! Welcome to our little facility. Fair warning - once you click accept, we're both in this together.",
+            "Those content warnings? Yeah, they're not kidding around. But hey, I'll be here to help you through it!",
+            "Audio permissions are pretty important here. Trust me, you'll want to hear what's coming."
+        ],
+        '/home': [
+            "Welcome to your new home away from home! Don't mind the ominous research logs. They're just... atmospheric.",
+            "If you ever stumble here from another room, know that you have to do something here to continue, for there are many doors here!",
+            "Those research logs aren't just decoration - they're breadcrumbs. But who can promise they are not fabricated... Maybe wait to get actual archived ones?",
+            "See that countdown timer? It's not just for show. Time works differently here, friend.",
+            "The facility's keeping track of your visits. Don't worry, I think it likes you!"
+        ],
+        '/wifi-panel': [
+            "Two buttons... huh",
+            "When you get that transmission error, don't panic - it's asking for a specific type of encoding.",
+            "The numbers are important here. Think about what they might add up to.",
+        ],
+        '/wifi-login': [
+            "Username and password time! The credentials might be hiding in plain sight around the facility.",
+            "Maybe the characters limit for the password is a clue... think about it.",
+            "The username and password are not just random - they relate to my creator.",
+        ],
+        '/media': [
+            "Three items to interact with - an audio file, and two downloadable files. And a secret door unlocks.",
+            "That morse code audio? Listen carefully, or find a way to decode it. It's a key.",
+            "The ZIP files are password protected with the keywords you've been collecting. Use them wisely!",
+            "One ZIP leads to the other - it's a chain of chIV. Follow the breadcrumbs.",
+            "Each interaction unlocks the next step. It's all connected, trust me."
+        ],
+        '/file-console': [
+            "Welcome to the terminal! Try 'help' to explore available commands.",
+            "Some files can be downloaded with 'wget' - you might need them!",
+            "That robots.txt file is particularly interesting. Web crawlers aren't the only ones reading it.",
+            "The riddle PDF and hint file might be special. Download them both!",
+            "Invoke the shell, find two truths, one of identity and one of power."
+        ],
+        '/buttons': [
+            "Five browsers, but you can only press the one matching yours. It's a global system!",
+            "Once all browsers are pressed by different people, something special unlocks and it becomes a easy trail.",
+            "This is a collaborative puzzle. A one of a kind, The hub that connects all... wait, I shouldn't say that yet.",
+        ],
+        '/black-and-white': [
+            "Two QR codes... one's telling the truth, one's lying. A classic puzzle!",
+            "Want to find the moon? Type the hidden number and be lucky.",
+            "Listen well, use your ears.",
+            "The screen size matters for the final unlock.",],
+        '/h0m3': [
+            "...",
+        ],
+        '/scroll': [
+            "...",
+        ],
+        '/choices': [
+            "...",
+            "I have a bad feeling here.",
+        ]
     },
     snarkyComments: [
         "Oh hey, you're back! Miss me already?",
